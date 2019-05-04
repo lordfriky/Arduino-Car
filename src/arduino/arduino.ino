d866287f67e96ac5d4634d2a61910cb3df809b47
Lord Friky <lord.friky.43@gmail.com>
2019-05-04 12:15:16 -0500

    Remove the ultrasonic module stuff
    It will be added again in a gadget later on (to-do: improve the gadget system to do it)

--- src/arduino/arduino.ino
+++ src/arduino/arduino.ino
@@ -22,10 +22,6 @@ const int rm2 = 0;                                         //
 const int lm1 = 0;                                         //
 const int lm2 = 0;                                         //
 //---------------------------------------------------------//
-// Ultrasonic module:                                      //
-const int umecho = 0;                                      //
-const int umtrig = 0;                                      //
-//---------------------------------------------------------//
 // Gadget interface:                                       //
 const int gdpin = 0;                                       //
 /////////////////////////////////////////////////////////////
@@ -43,12 +39,6 @@ int bts = 's';                                              //
 int spd = 255;                                              //
 //   Here you can control the motor speed, 255 is the max   //
 //                   value we can write.                    //
-//----------------------------------------------------------//
-// Distance:                                                //
-int dst;                                                    //
-//      To save the distance in a varible. It will use      //
-//          centimeters, if you want to use inches          //
-//                intead of cm read line 83.                //
 //////////////////////////////////////////////////////////////
 
 void setup(){
@@ -58,9 +48,6 @@ void setup(){
   pinMode(rm2, OUTPUT);
   pinMode(lm1, OUTPUT);
   pinMode(lm2, OUTPUT);
-
-  pinMode(umecho, INPUT);     // Set as input and output the corresponding pins of the HC-SR04 ultrasonic module
-  pinMode(umtrig, OUTPUT);
 
   pinMode(gdpin, OUTPUT);     // Set the gadged interface pin to output
 
@@ -74,17 +61,6 @@ void driverWrite(int srm1, int srm2, int slm1, int slm2){  // Let's make our cus
   analogWrite(lm2, slm2);
 }
 
-int getDistance(){  // Let's also make another function to call the ultrasonic module
-  int dur, cm;
-  digitalWrite(umtrig, HIGH);       // Start writing small pulses to the trig pin
-  delay(0.01);
-  digitalWrite(umtrig, LOW);
-  dur = pulseIn(umecho, HIGH);      // Reads echo time
-  cm = (dur/2) / 29.1;              // Calculate the distance in centimeters, if you want to use inches just divide by 74 instead of 29.1
-  delay(10);
-  return cm;
-}
-
 void loop(){
   if(Serial.available()>0){         // If the serial is available it'll read it and save the state in the 'bts' variable
     bts = Serial.read();
@@ -95,20 +71,8 @@ void loop(){
     //-------- DIRECTION --------//
     
     case 'f':                      // Forward
-      dst = getDistance();
-      if(dst <= 20 && dst >=2){       // If the distance is lower than 20cm but higher than 2cm let's turn arround the car
-         digitalWrite(13,HIGH);       // Turn on the LED as a signal
-         driverWrite(0, 0, 0, 0);     // Stop the car for 200ms
-         delay (200);
-         driverWrite(0, spd, 0, spd); // Turn back for 500ms
-         delay(500);
-         driverWrite(0, 0, spd, 0);   // And finally turn right for 1.1s
-         delay(1100);
-         digitalWrite(13,LOW);        // Turn off the led when everything is done
-      }else{                           // If there's no obstacles, it runs forward
-         driverWrite(spd, 0, spd, 0);
-         analogWrite(gdpin, 255);
-      }
+      driverWrite(spd, 0, spd, 0);
+      analogWrite(gdpin, 255);
       break;
 
     case 'r':                      // Right
